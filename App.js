1318aaf61e72d465a7315b673e2988339b7a9acc
ltkiet14 <dohuyhoang1907@gmail.com>
2024-05-07 20:14:30 +0700

    up

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { MyContextControllerProvider } from './src/context/';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Router from './src/screens/Router';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <PaperProvider>
+      <MyContextControllerProvider>
+        <NavigationContainer>
+          <Router />
+        </NavigationContainer>
+      </MyContextControllerProvider>
+    </PaperProvider>
+  );
+}
+
+export default App;
